cd62b0ff463ed714488e8e19619468a8adbb64d8
Javier Zaldívar Coto <javierzc051597@gmail.com>
2017-08-14 14:02:26 -0600

    Clases de apoyo
    Curso y ciclo son para poder trabajar la clase grupo

---
+++ BackEnd/src/backend/entidades/Ciclo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package backend.entidades;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Ciclo {
+    private String codigo;
+    
+    public Ciclo() {
+        codigo = "1";
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+}
