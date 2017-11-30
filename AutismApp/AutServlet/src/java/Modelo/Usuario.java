7759e86f211dc86db3b7c7885348c40858605422
Pablex223 <psolano223@gmail.com>
2017-11-29 20:54:19 -0600

    Model

---
+++ AutismApp/AutServlet/src/java/Modelo/Usuario.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author Pablo
+ */
+@Entity
+@Table(name = "usuario")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Usuario.findAll", query = "SELECT u FROM Usuario u")
+    , @NamedQuery(name = "Usuario.findByUsuario", query = "SELECT u FROM Usuario u WHERE u.usuario = :usuario")
+    , @NamedQuery(name = "Usuario.findByContra", query = "SELECT u FROM Usuario u WHERE u.contra = :contra")})
+public class Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "usuario")
+    private String usuario;
+    @Basic(optional = false)
+    @Column(name = "contra")
+    private String contra;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "loginusuario")
+    private List<Persona> personaList;
+
+    public Usuario() {
+    }
+
+    
+    
+    
+    public Usuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public Usuario(String usuario, String contra) {
+        this.usuario = usuario;
+        this.contra = contra;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContra() {
+        return contra;
+    }
+
+    public void setContra(String contra) {
+        this.contra = contra;
+    }
+
+    @XmlTransient
+    public List<Persona> getPersonaList() {
+        return personaList;
+    }
+
+    public void setPersonaList(List<Persona> personaList) {
+        this.personaList = personaList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (usuario != null ? usuario.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Usuario)) {
+            return false;
+        }
+        Usuario other = (Usuario) object;
+        if ((this.usuario == null && other.usuario != null) || (this.usuario != null && !this.usuario.equals(other.usuario))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Modelo.Usuario[ usuario=" + usuario + " ]";
+    }
+    
+    
+    public org.json.JSONObject toJson(){
+        return new org.json.JSONObject()
+                .put("usuario", this.usuario)
+                .put("contra", this.contra);
+    } 
+    
+    public static Usuario fromJson(org.json.JSONObject jo)
+            throws org.json.JSONException
+    {
+        return new Usuario(
+                jo.getString("usuario"),
+                jo.getString("contra")
+                
+                               
+            );  
+}
+    
+    
+}
